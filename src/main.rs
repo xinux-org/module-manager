eb208bfa843fc665ee99058ac3ad4d3038d78a41
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-05-08 23:39:05 +0500

    fix: schemes

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ fn main() {
     // }
     // gtk::Window::set_default_icon_name(nix_software_center::config::APP_ID);
     // let app = adw::Application::new(Some(nix_software_center::config::APP_ID), gio::ApplicationFlags::empty());
-    // app.set_resource_base_path(Some("/dev/vlinkz/NixSoftwareCenter"));
+    // app.set_resource_base_path(Some("/org/xinux/NixSoftwareCenter"));
     let app = RelmApp::new("org.xinux.modulemanager");
 
     match load() {

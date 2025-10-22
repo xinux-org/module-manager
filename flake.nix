e5958ae2ad0557fd31b17eb4658586390793002f
bahrom04 <magdiyevbahrom@gmail.com>
2025-10-22 14:55:27 +0500

    feat: add hydraJobs

--- flake.nix
+++ flake.nix
@@ -1,17 +1,20 @@
 {
   inputs = {
-    nixpkgs.url = "github:xinux-org/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:xinux-org/nixpkgs/nixos-unstable";
     xinux-lib = {
       url = "github:xinux-org/lib";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs:
+  outputs = {self, ...} @ inputs:
     inputs.xinux-lib.mkFlake {
       inherit inputs;
       alias.packages.default = "xinux-module-manager";
       alias.shells.default = "xinux-module-manager";
       src = ./.;
+      hydraJobs = {
+        inherit (self.packages.x86_64-linux) default;
+      };
     };
 }

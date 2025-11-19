767c9516f0d1a128b64317d3a1e68f3db3657cf5
bahrom04 <magdiyevbahrom@gmail.com>
2025-11-19 18:32:13 +0500

    chore: add loading resources

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,32 @@
-use gtk::glib;
-
-use log::error;
-use relm4::*;
-use xinux_module_manager::ui::{
-    load::load,
-    window::{AppInit, AppModel},
+use log::{error, info};
+use relm4::{
+    gtk::{gio, glib, prelude::ApplicationExt},
+    RelmApp, *,
 };
+use xinux_module_manager::{
+    config::{self, RESOURCES_FILE},
+    ui::{
+        load::load,
+        window::{AppInit, AppModel},
+    },
+};
+
 fn main() {
     gtk::init().unwrap();
     pretty_env_logger::init();
+
     glib::set_application_name("Module Manager");
-    // if let Ok(res) = gio::Resource::load(RESOURCES_FILE) {
-    //     info!("Resource loaded: {}", RESOURCES_FILE);
-    //     gio::resources_register(&res);
-    // } else {
-    //     error!("Failed to load resources");
-    // }
-    // gtk::Window::set_default_icon_name(nix_software_center::config::APP_ID);
-    // let app = adw::Application::new(Some(nix_software_center::config::APP_ID), gio::ApplicationFlags::empty());
-    // app.set_resource_base_path(Some("/org/xinux/NixSoftwareCenter"));
+    gtk::Window::set_default_icon_name(config::APP_ID);
+
+    if let Ok(res) = gio::Resource::load(RESOURCES_FILE) {
+        info!("Resource loaded: {}", RESOURCES_FILE);
+        gio::resources_register(&res);
+    } else {
+        error!("Failed to load resources");
+    }
+
+    let app = adw::Application::new(Some(config::APP_ID), gio::ApplicationFlags::empty());
+    app.set_resource_base_path(Some("/org/xinux/XinuxModuleManager"));
     let app = RelmApp::new("org.xinux.modulemanager");
 
     match load() {
